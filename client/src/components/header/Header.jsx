e78af36c0b20a7d63e28cad7b82b6e60b79d3b3b
Nguyen Duc Thinh <thinhnnguyenndt248@gmail.com>
2025-04-17 16:48:06 +0700

    update

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -68,7 +68,7 @@ const Header = () => {
               Hotel, car and experiences
             </h1>
             <p className="headerDesc">
-              Accompanying us, you have a trip full of experiences. With Chisfis, booking accommodation, resort villas
+              Accompanying us, you have a trip full of experiences. With Chisfis, booking accommodation
             </p>
           </div>
           <div className="headerImages">
